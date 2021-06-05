e1d84b4489ad7721a6f6f713298580b7fa70dde0
Akanksha Singh <akankshasingh121297@gmail.com>
2021-06-05 20:22:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 var noTextError = new Error('You must highlight a text in order to search!'); 
 
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
 var searchGoogle = function(word){
     var query = word.selectionText;
-    if (query != undefined){chrome.tabs.create({url: "https://www.google.com/search?q=" + query});}
-    else{alert('Highlight a text in order to search!');}
+    if (query != undefined){
+        chrome.tabs.query({
+    active: true,
+    currentWindow: true
+    }, function(tabs) {        
+    chrome.tabs.create({url: "https://www.google.com/search?q=" + query});}
+    });
+        
 };
+
+
 
 chrome.runtime.onInstalled.addListener
 (
